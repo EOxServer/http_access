1ab0b3ac10cb777f68c2ece5c3815d39f2c8ba29
jankovicn <nikola.jankovic@eox.at>
2021-05-10 11:17:26 +0200

    updating error handling added boundaries

---
+++ http_access/utils.py
@@ -0,0 +1,107 @@
+import string
+import random
+import re
+from typing import Generator, List, Tuple, Union
+
+from eoxserver.resources.coverages import models as coverages
+from eoxserver.backends.access import vsi_open
+import eoxserver.core.util.multiparttools as mp
+
+class RangeNotSatisfiable(Exception):
+    pass
+
+def iter_file(data_item: coverages.ArrayDataItem, ranges: List[Tuple]=None, chunk_size: int=65000) -> Generator:
+    with vsi_open(data_item) as r:
+        if ranges:
+            for _r in ranges:
+                bottom = _r[0]
+                top = _r[1]
+
+                if bottom and top:
+                    r.seek(bottom, 0)
+                    while bottom < top:
+                        yield r.read(min(top - bottom, chunk_size))
+                        bottom += chunk_size
+                elif bottom and not top:
+                    r.seek(bottom, 0)
+                    while True:
+                        data = r.read(chunk_size)
+                        if not data:
+                            break
+                        yield data
+
+                elif not bottom and top:
+                    r.seek(-top, 2)
+                    while True:
+                        data = r.read(chunk_size)
+                        if not data:
+                            break
+                        yield data
+                else:
+                    continue
+        else:
+            while True:
+                data = r.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+def parse_ranges(ranges: Union[str, None]) -> Tuple[str, List[Tuple[Union[int, None]]]]:
+    if ranges:
+        ranges = re.match(r'bytes=((\d*-\d*,?)+)', ranges)
+        if not ranges:
+            raise RangeNotSatisfiable('Requested Range Not Satisfiable')
+
+        ranges = ranges.group()
+        units, ranges  = ranges.split('=')
+
+        range_list = []
+        ranges = ranges.split(',')
+        for range in ranges:
+            start_index, end_index = range.split('-')
+            try:
+                start_index = int(start_index)
+            except ValueError:
+                start_index = None
+            try:
+                end_index = int(end_index)
+            except ValueError:
+                end_index = None
+
+            if (start_index and end_index) and start_index > end_index:
+                raise RangeNotSatisfiable('Requested Range Not Satisfiable')
+            if not start_index and not end_index:
+                raise RangeNotSatisfiable('Requested Range Not Satisfiable')
+            range_list.append((start_index, end_index))
+
+
+        return units, range_list
+    
+    return None, None
+
+def wrap_streaming_content(content: map, ranges: List[Tuple], boundary: str, size: str='*') -> Generator:
+    for chunk, _r in zip(content, ranges):
+        yield wrap_chunk(chunk, _r, boundary, size)
+    yield b'--' + boundary + b'--'
+
+def wrap_chunk(chunk: bytes, _r: Tuple[int], boundary: str, size: str='*') -> bytes:
+    type = b'Content-Type: application/octet-stream\n'
+    range = f'Content-Range: bytes {_r[0] or ""}-{_r[1] or ""}/{size}\n'.encode()
+    chunk = b'--' + boundary + b'\n' + type + range + b'\n' + chunk + b'\n'
+    return chunk
+
+def generate_boundary() -> str:
+    characters = string.ascii_lowercase + string.digits
+    result_str = ''.join(random.sample(characters, 8))
+    return str.encode(result_str)
+
+def verify_file(data_item, ranges: List[Tuple]):
+    with vsi_open(data_item) as r:  
+        r.seek(0, 2)
+        size = r.tell()
+        for _r in ranges:
+            bottom = _r[0]
+            if bottom and bottom > size:
+                raise RangeNotSatisfiable("Range Not Satisfiable")
+
+    return str(size)
--- http_access/views.py
+++ http_access/views.py
@@ -1,109 +1,49 @@
-from io import BytesIO
-import json
 from typing import Union
 
-import boto3
-import requests
-from keystoneauth1 import session
-from keystoneauth1.identity import v3
-from swiftclient.client import Connection
-from swiftclient.exceptions import ClientException
-from botocore import UNSIGNED
-from botocore.exceptions import ClientError
-from botocore.config import Config
-
-from django.http import HttpResponse, HttpResponseNotFound, StreamingHttpResponse
+from django.http import HttpResponseNotFound, StreamingHttpResponse, HttpResponse
 from django.http.response import Http404
 from django.shortcuts import get_object_or_404
 
 from eoxserver.backends import models as backends
 from eoxserver.resources.coverages import models as coverages
-from eoxserver.backends.access import vsi_open
 
-def get_s3_file(path: str, _range: str, storage: backends.Storage) -> Union[HttpResponse, HttpResponseNotFound, StreamingHttpResponse]:
-    storage_auth = json.loads(storage.storage_auth.auth_parameters)
-    bucket = storage.url
+from .utils import parse_ranges, iter_file, wrap_streaming_content, generate_boundary, RangeNotSatisfiable, verify_file
 
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=storage_auth["ACCESS_KEY_ID"],
-        aws_secret_access_key=storage_auth["SECRET_ACCESS_KEY"],
-        config=Config(signature_version=UNSIGNED) if storage_auth.get("PUBLIC", False) else None,
-    )
-
-    try:
-        if _range:
-            resp = s3.get_object(Bucket=bucket, Key=path, Range=_range)
-            b = BytesIO(resp["Body"].read())
-            response = HttpResponse(b, content_type="application/octet-stream")
-        else:
-            url = s3.generate_presigned_url(
-                ClientMethod="get_object", 
-                ExpiresIn=3600,
-                Params={
-                    "Bucket": bucket,
-                    "Key": path,
-                },
-            )
-            r = requests.get(url=url, stream=True)
-            r.raise_for_status()
-            response = StreamingHttpResponse(r.raw, content_type="application/octet-stream")
-    except (IOError, ClientError):
-        response = HttpResponseNotFound("<h1>File not found</h1>")
-
-    return response
-
-
-def get_swift_file(path: str, _range: str, storage: backends.Storage) -> Union[HttpResponse, HttpResponseNotFound]:
-
-    bucket = storage.url
-    storage_auth = storage.storage_auth
-    url = storage_auth.url
-    storage_auth = json.loads(storage.storage_auth.auth_parameters)
-    auth = v3.Password(
-        auth_url=url,
-        username=storage_auth["username"],
-        password=storage_auth["password"],
-        # user_domain_name="Default",
-        # project_name="test",
-        # project_domain_name="Default",
-    )
-    keystone_session = session.Session(auth=auth)
-    swift_conn = Connection(session=keystone_session)
-
-    try:
-        if _range:
-            _, obj = swift_conn.get_object(bucket, path, headers={"Range": _range})
-            b = BytesIO(obj)
-            response = HttpResponse(b, content_type="application/octet-stream")
-        else:
-            _, obj = swift_conn.get_object(bucket, path)
-            response = HttpResponse(obj, content_type="application/octet-stream")
-    except (IOError, ClientException):
-        response = HttpResponseNotFound("<h1>File not found</h1>")
-    return response
-
-
-def return_file(request, storage_name: str, path: str) -> Union[HttpResponse, HttpResponseNotFound]:
-    import debugpy
-    debugpy.listen(("0.0.0.0", 5678))
-    debugpy.wait_for_client()
+def return_file(request, storage_name: str, path: str) -> Union[HttpResponseNotFound, StreamingHttpResponse, HttpResponse]:
 
     try:
         storage = get_object_or_404(backends.Storage, name=storage_name)
-        _ = get_object_or_404(coverages.ArrayDataItem, location=path)
-        _range = request.headers.get("Range", None)
+        data_item = get_object_or_404(coverages.ArrayDataItem, location=path, storage=storage)
+        ranges = request.headers.get("Range", None)
+        _, ranges = parse_ranges(ranges)
+
+
+        if request.method == 'GET':
+            if ranges:
+                size = verify_file(data_item, ranges)
+            response = StreamingHttpResponse(iter_file(data_item, ranges=ranges))
+            if not ranges:
+                response['Content-Type'] = 'image/tiff'
+            else:
+                boundary = generate_boundary()
+                response.streaming_content = wrap_streaming_content(response.streaming_content, ranges, boundary, size)
+                response['Content-Type'] = f'multipart/byteranges; boundary={boundary.decode()}'
+                response.status_code = 206
+        else:
+            response = HttpResponse('<h1>Test<h1>')
+
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Accept-Ranges'] = 'bytes'
+        response['Access-Control-Allow-Methods']= 'POST, GET, OPTIONS'
+        response['Access-Control-Allow-Headers']= 'X-PINGOTHER, Content-Type'
+        response['Access-Control-Max-Age']= '86400'
+
+        return response
 
     except Http404:
-        return HttpResponseNotFound("<h1>File not found</h1>")
-    
-    storage_type = storage.storage_type
-    try:
-        if storage_type.lower() == "swift":
-            return get_swift_file(path, _range, storage)
-        elif storage_type.lower() == "s3":
-            return get_s3_file(path, _range, storage)
-        else:
-            return HttpResponseNotFound("<h1>File not found</h1>")
-    except Exception as e:
-        return HttpResponse(e, status=500)
+        return HttpResponseNotFound("<h1>404 file not found</h1>")
+
+    except RangeNotSatisfiable:
+        response = HttpResponse('<h1>416 requested range not satisfiable<h1>')
+        response.status_code = 416
+        return response
