d0ec3b82a6662d4871f61dcaba95347225484994
jankovicn <nikola.jankovic@eox.at>
2022-01-18 11:59:35 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup, find_packages
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-long_description = ""
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="http_access",
     version="0.0.1",
-    author="",
-    author_email="",
+    author="Nikola Jankovic",
+    author_email="nikola.jankovic@eox.at",
     description="http direct access django app",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.eox.at/esa/prism/vs/-/tree/master/core",
+    url="https://github.com/EOxServer/http_access",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
